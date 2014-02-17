74510e3458e14a51fa48b02ff85352f0fb36661a
Chris Roberts <chrisroberts.code@gmail.com>
2014-02-17 11:00:41 -0800

    Only take action on deb type files. Only add key id option if custom id is provided

--- lib/reaper/signer/deb.rb
+++ lib/reaper/signer/deb.rb
@@ -2,13 +2,19 @@ module Reaper
   class Signer
     module Deb
 
-      def package(*packages)
-        verify_type!(packages)
-        packages.each_slice(sign_chunk_size) do |pkgs|
+      def package(*pkgs)
+        pkgs = valid_packages(*pkgs)
+        pkgs.each_slice(sign_chunk_size) do |pkgs|
           opts = ["--sign='#{sign_type}'"]
-          opts.push("--default-key='#{key_id}'")
+          opts.push("--default-key='#{key_id}'") if key_id
           cmd = (['debsigs'] + opts + pkgs).join(' ')
           shellout!(cmd)
+        end
+      end
+
+      def valid_packages(*pkgs)
+        pkgs.find_all do |pkg|
+          File.extname(pkg) == '.deb'
         end
       end
 
