62a4bf5fc16679a373d4d7b7f47a7d75efc2d6c9
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-04 14:04:24 -0700

    Include package list processor implemention details for gem pacakges

---
+++ lib/reaper/package_list/gem.rb
@@ -0,0 +1,58 @@
+require 'rubygems/package'
+
+module Reaper
+  class PackageList
+    class Processor
+      class Gem < Processor
+
+        def initialize(*_)
+        end
+
+        def add(hash, package)
+          info = extract_fields(package)
+          filenames = inject_package(hash, info, package)
+          filenames
+        end
+
+        def remove(hash, package_name, version, args={})
+          deleted = false
+          if(hash['rubygems'][package_name])
+            if(version)
+              deleted = hash['rubygems'][package_name].delete(version)
+            else
+              deleted = hash['rubygems'].delete(package_name)
+            end
+          end
+          !!deleted
+        end
+
+        def extract_fields(package)
+          spec = Gem::Package.open(File.open(package)){|pack| pack.metadata}
+          Rash[
+            spec.to_yaml_properties.map do |var_name|
+              [var_name.to_s.tr('@', ''), spec.instance_variable_get(var_name)]
+            end
+          ]
+        end
+
+        def inject_package(hash, info, package)
+          package_path = File.join(
+            'rubygems', 'gems', "#{info['name']}-#{info['version']}.gem"
+          )
+          classification = info['version'].prerelease? ? 'prerelease' : 'release'
+          hash.deep_merge!(
+            'rubygem' => {
+              classification => {
+                info['name'] => {
+                  info['version'].to_s => info.merge(:package_path => package_path)
+                }
+              }
+            }
+          )
+          package_path
+        end
+
+      end
+    end
+  end
+end
