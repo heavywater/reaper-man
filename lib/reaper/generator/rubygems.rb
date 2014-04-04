6d34d1ab7f3a1bcecbf6c437efcd11d25f9ade07
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-04 14:04:50 -0700

    Include generator for building gems compat layout

---
+++ lib/reaper/generator/rubygems.rb
@@ -0,0 +1,82 @@
+require 'time'
+
+module Reaper
+  class Generator
+    module Rubygems
+
+      def generate!
+        generate_gemstore(package_config[:rubygems])
+      end
+
+      def generate_gemstore(gems)
+        generate_indexing(gems)
+        generate_specs(gems)
+      end
+
+      def generate_indexing(gems)
+        build_spec_file('specs', gems.fetch(:release, {}))
+        build_spec_file('latest_specs', gems.fetch(:release, {}))
+        build_spec_file('prerelease', gems.fetch(:prerelease, {}))
+        write_quick_specs(gems.fetch(:release, {}))
+        write_quick_specs(gems.fetch(:prerelease, {}))
+      end
+
+      def create_index(gems)
+        [].tap do |list|
+          gems.each do |name, all|
+            all.each do |version, info|
+              list << [name, Gem::Version.new(version), info[:platform]]
+            end
+          end
+        end
+      end
+
+      def marshal_path
+        ['Marshal', marshal_version].join('.')
+      end
+
+      def marshal_version
+        [Marshal::MAJOR_VERSION, Marshal::MINOR_VERSION].join('.')
+      end
+
+      def build_spec_file(name, gems)
+        index = create_index(gems)
+        create_file("#{name}.#{marshal_version}") do |file|
+          file.write(Marshal.dump(index))
+        end
+        compress_file("#{name}.#{marshal_version}")
+      end
+
+      def write_quick_specs(gems)
+        gems.each do |name, list|
+          list.each do |version, info|
+            spec = Gem::Specification.new(name)
+            info.each do |var, value|
+              if(spec.respond_to?("#{var}="))
+                begin
+                  spec.send("#{var}=", value)
+                rescue Gem::InvalidSpecificationException => e
+                  # TODO: Do we have a logger in this project?
+                end
+              end
+            end
+            spec.version = Gem::Version.new(info[:version])
+            spec.date = Time.parse(info[:date])
+            create_file('quick', marshal_path, "#{name}-#{version}.gemspec.rz") do |file|
+              file.write(Marshal.dump(spec))
+            end
+          end
+        end
+      end
+
+      def sign_file_if_setup
+        path = yield
+        if(signer && options[:sign])
+          signer.file(path)
+        end
+        path
+      end
+
+    end
+  end
+end
