6a75ce0c2f6453c44904455bc78b7f8b11fd9de6
Chris Roberts <code@chrisroberts.org>
2015-06-01 10:33:00 -0700

    Remove deprecated items

--- lib/reaper-man.rb
+++ lib/reaper-man.rb
@@ -6,11 +6,7 @@ module ReaperMan
   autoload :Error, 'reaper-man/error'
   autoload :Generator, 'reaper-man/generator'
   autoload :PackageList, 'reaper-man/package_list'
-  autoload :Rash, 'reaper-man/rash'
-  autoload :Runner, 'reaper-man/runner'
   autoload :Signer, 'reaper-man/signer'
   autoload :Version, 'reaper-man/version'
   autoload :Utils, 'reaper-man/utils'
 end
-
-require 'reaper-man/rash'
