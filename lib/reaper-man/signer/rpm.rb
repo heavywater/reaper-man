d14ab89b21aa140db2dbd2d4b78e57e2307e3c0d
Chris Roberts <code@chrisroberts.org>
2015-07-17 13:52:12 -0700

    Include stub rpm signer to prevent load errors

---
+++ lib/reaper-man/signer/rpm.rb
@@ -0,0 +1,18 @@
+require 'reaper-man'
+
+module ReaperMan
+  class Signer
+    # Signing methods for deb files
+    module Rpm
+
+      def package(*pkgs)
+        nil
+      end
+
+      def valid_packages(*pkgs)
+        nil
+      end
+
+    end
+  end
+end
