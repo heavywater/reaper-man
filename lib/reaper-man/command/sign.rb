8dee59298cd88f049d574c41205adfb442775bf8
Chris Roberts <code@chrisroberts.org>
2015-07-17 07:12:37 -0700

    Allow files or directories to be passed for signing

--- lib/reaper-man/command/sign.rb
+++ lib/reaper-man/command/sign.rb
@@ -5,14 +5,26 @@ module ReaperMan
     class Sign < Command
 
       def execute!
+        files = arguments.map do |item|
+          if(File.file?(item))
+            item
+          else
+            File.directory?(item)
+            i_files = Dir.glob(File.join(item, '**', '*'))
+            i_files.delete_if do |path|
+              !File.file?(path)
+            end
+            i_files
+          end
+        end.flatten.compact.uniq
         run_action 'Signing file(s)' do
           signer = Signer.new(config)
-          files = Dir.glob(File.join(arguments.first, '**', '*'))
-          files.delete_if do |path|
-            !File.file?(path)
-          end
           signer.package(*files)
           nil
+        end
+        ui.info "Files signed:"
+        files.sort.each do |path|
+          ui.puts "  #{ui.color(File.expand_path(path), :yellow)}"
         end
       end
 
